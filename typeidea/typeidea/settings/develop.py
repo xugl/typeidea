2253f53d6656425185d157b7b8e6115bb6680db7
xugl <xugl@163.com>
2018-08-25 15:32:13 +0800

    init project

---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+
+from  .base import *   # noqa
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# INSTALLED_APPS += [
+#     'debug_toolbar',
+# ]
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
