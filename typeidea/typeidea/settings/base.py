b74aa1ea9c1a65124bb0ba59b63d537e9c60905e
xugl6 <xugl6@asiainfo.com>
2018-10-07 18:14:10 +0800

    静态页面

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -97,9 +97,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
 
 THEME = 'themes/default'
+#STATIC_ROOT='/tmp/static'
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,THEME,"static"),
+]
 
 TEMPLATES = [
     {
