38b6eceb3a5bdde2fd89987ce7d1c0c98625381e
xugl6 <xugl6@asiainfo.com>
2018-10-20 11:08:22 +0800

    抽取评论代码

--- typeidea/comment/forms.py
+++ typeidea/comment/forms.py
@@ -6,16 +6,44 @@ from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    nickname = forms.CharField(
+        label='昵称',
+        max_length=50,
+        widget=forms.widgets.Input(
+            attrs={'class': 'form-control'}
+        )
+    )
+
+    email = forms.CharField(
+        label='Email',
+        max_length=50,
+        widget=forms.widgets.EmailInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+
+    website = forms.CharField(
+        label='网站',
+        max_length=100,
+        widget=forms.widgets.URLInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+
     content = forms.CharField(
         label='内容',
         max_length=100,
-        widget=forms.widgets.Textarea(attrs={'rows':6,'cols':80})
+        widget=forms.widgets.Textarea(
+            attrs={'class': 'form-control'}
+        )
     )
 
     def clean_content(self):
         content = self.cleaned_data.get('content')
         if len(content) < 10:
             raise forms.ValidationError('长度怎么能这么短呢？')
+
+        return content
 
     class Meta:
         model = Comment
--- typeidea/comment/views.py
+++ typeidea/comment/views.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
+from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .forms import CommentForm
 from comment.models import Comment
 from django.shortcuts import redirect
-
 
 class CommentView(TemplateView):
     template_name = 'comment/result.html'
@@ -14,6 +13,7 @@ class CommentView(TemplateView):
     def post(self,request,*args,**kwargs):
         comment_form = CommentForm(request.POST)
         target = request.POST.get('target')
+
         if comment_form.is_valid():
             instance = comment_form.save(commit=False)
             instance.target = target
@@ -21,9 +21,8 @@ class CommentView(TemplateView):
             succeed = True
         else:
             succeed = False
-
         context = {
-            'success': succeed,
+            'succeed': succeed,
             'form': comment_form,
         }
         return self.render_to_response(context)
